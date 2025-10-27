fe637ac61c816ad66dbe02f557efebb5eb5028ee
lolo <root@sltcestloic.dev>
2025-10-27 08:29:15 +0100

    return null if size 0

--- src/malloc.c
+++ src/malloc.c
@@ -5,13 +5,14 @@
 pthread_mutex_t g_malloc_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *malloc(size_t size) {
-	write(1, "malloc called\n", 14);
 	void *ptr = NULL;
 	pthread_mutex_lock(&g_malloc_mutex);
 	size = (size + 15) & ~15;
-	char buf[32];
-	int len = snprintf(buf, sizeof(buf), "malloc(%zu)\n", size);
-	write(1, buf, len);
+	if (size == 0) {
+		pthread_mutex_unlock(&g_malloc_mutex);
+		return NULL;
+	}
+	
 	pthread_mutex_unlock(&g_malloc_mutex);
 	return ptr;
 }
